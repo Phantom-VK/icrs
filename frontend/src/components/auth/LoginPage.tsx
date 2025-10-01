36f0b9c329ca71ffa3d212be2c857dd1c2c2083f
Vedant Kalandre <124573857+vedantVK-1904@users.noreply.github.com>
2025-10-01 16:47:03 +0530

    Update LoginPage.tsx

--- frontend/src/components/auth/LoginPage.tsx
+++ frontend/src/components/auth/LoginPage.tsx
@@ -3,7 +3,7 @@ import { TextField, Button, Card, CardContent, Typography, Box } from "@mui/mate
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import logo from "../../assets/logo.png";
-import authService from "../../services/authService"; // adjust path for dummy api
+import authService from "../../services/authService"; 
 
 
 const LoginPage: React.FC = () => {
@@ -67,7 +67,7 @@ const LoginPage: React.FC = () => {
           {/* Create Account */}
           <Button
             fullWidth
-            variant="contained"      // same style as Sign In
+            variant="contained"    
             sx={{ mt: 1, backgroundColor: "#0288d1", "&:hover": { backgroundColor: "#0277bd" } }}
             onClick={() => navigate("/auth/create-account")}
           >
